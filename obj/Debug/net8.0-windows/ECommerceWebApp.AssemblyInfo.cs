4cd8d9e07b4683e83a981987b27e8fb9c19d94cc
muhammed tarık mengüç <108359345+tarikmenguc@users.noreply.github.com>
2025-01-14 01:35:49 +0300

    a
    a

--- obj/Debug/net8.0-windows/ECommerceWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/ECommerceWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36cea3bd037bf3554dcea0f18260cdb25a4fe078")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
